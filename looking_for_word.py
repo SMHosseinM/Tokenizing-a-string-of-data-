75a654087441cb9f3b2b9f6c8730c7e7d9e18b86
Mohammad Hossein <smh.miraftabi1373@gmail.com>
2018-02-11 17:58:46 +0000

    completing the code using example

--- looking_for_word.py
+++ looking_for_word.py
@@ -1,7 +1,13 @@
-def count(sequence, item)
+def count(sequence, item):
 	total = 0 #the number of times the word is repeated
 	if type(sequence) == list:
 		for element in sequence:
-			if element == item
+			if element == item:
 				total+=1
-	print('The number of matches are: ' total)
+	print('The number of matches are: ', total)
+
+#actual code
+from nltk.tokenize import wordpunct_tokenize
+string = "Hey bad body. you are bad bad bad. good bye bad"
+tok = wordpunct_tokenize(string)
+count(tok, 'bad')
