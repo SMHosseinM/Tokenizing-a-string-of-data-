41453158c0fb257c72488f11caeb857a67f9c355
Hossein <smh.miraftabi1373@gmail.com>
2018-02-11 12:49:55 +0000

    the firt draft for searching a intended word in python

---
+++ looking_for_word.py
@@ -0,0 +1,7 @@
+def count(sequence, item)
+	total = 0 #the number of times the word is repeated
+	if type(sequence) == list:
+		for element in sequence:
+			if element == item
+				total+=1
+	print('The number of matches are: ' total)
